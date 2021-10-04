e7e26c93c86111a4e279e0783df5d396a882b296
Nilesh R Mitra <Nilesh.Mitra@gds.ey.com>
2021-10-04 22:16:32 +0530

    Unit test case naming convention and Mock with setup

--- ApplicationLayer.NewWayOfAPI.UnitTest/UnitTestForSurveyPeriodService.cs
+++ ApplicationLayer.NewWayOfAPI.UnitTest/UnitTestForSurveyPeriodService.cs
@@ -8,17 +8,25 @@ using DomainLayer.NewWayOfAPI.Entities.SurveyPeriod;
 using DomainLayer.NewWayOfAPI.Entities.SurveyPeriod.Ports.Input;
 
 namespace ApplicationLayer.NewWayOfAPI.UnitTest
-{
+{    
     public class UnitTestForSurveyPeriodService
     {
+        private SurveyPeriodEntity surveyPeriodEntity;
+        private Mock<ISurveyPeriodDomainPortIP> surveyPeriodDomainPortIP;
+        private Mock<ISurveyPeriodRepository> surveyPeriodRepository;
+        private SurveyPeriodService surveyPeriodService;
+        public UnitTestForSurveyPeriodService()
+        {
+            surveyPeriodEntity = new SurveyPeriodEntity();
+            surveyPeriodDomainPortIP = new Mock<ISurveyPeriodDomainPortIP>();
+            surveyPeriodRepository = new Mock<ISurveyPeriodRepository>();
+            surveyPeriodService = new SurveyPeriodService(surveyPeriodDomainPortIP.Object, surveyPeriodRepository.Object);
+        }
         [Fact]
         public void Save_Survey_Period_Test_For_Exceptions()
         {
             //ARRANGE
-            var surveyPeriodEntity = new SurveyPeriodEntity();
-            var surveyPeriodDomainPortIP = new Mock<ISurveyPeriodDomainPortIP>();
-            var surveyPeriodRepository = new Mock<ISurveyPeriodRepository>();
-            var surveyPeriodService = new SurveyPeriodService(surveyPeriodDomainPortIP.Object, surveyPeriodRepository.Object);
+            //The arrangement has done in constructor
 
             //ACT
             //var ex = Assert.Throws<SurveyPeriodException>(() => surveyPeriodService.SaveSurveyPeriod(surveyPeriodEntity));
@@ -28,14 +36,12 @@ namespace ApplicationLayer.NewWayOfAPI.UnitTest
         }
 
         [Fact]
-        public void Save_Survey_Period_Test_For_Exceptions_Messages()
+        public void SaveSurveyPeriod_WhenSurveyPeriodIsNotUniqueAndSurveyPeriodIsNotValid_ShuouldRaiseException()
         {
             //ARRANGE
-            var surveyPeriodEntity = new SurveyPeriodEntity();
-            var surveyPeriodDomainPortIP = new Mock<ISurveyPeriodDomainPortIP>();
-            var surveyPeriodRepository = new Mock<ISurveyPeriodRepository>();
-            var surveyPeriodService = new SurveyPeriodService(surveyPeriodDomainPortIP.Object, surveyPeriodRepository.Object);
-
+            surveyPeriodDomainPortIP.Setup<bool>(x => x.IsSurveyPeriodUnique(It.IsAny<SurveyPeriodEntity>())).Returns(false);
+            surveyPeriodDomainPortIP.Setup<bool>(x => x.IsValidSurveyPeriod(It.IsAny<SurveyPeriodEntity>())).Returns(false);
+            
             //ACT
             var ex = Assert.Throws<SurveyPeriodException>(() => surveyPeriodService.SaveSurveyPeriod(surveyPeriodEntity));
 
@@ -43,5 +49,48 @@ namespace ApplicationLayer.NewWayOfAPI.UnitTest
             Assert.Equal("Invalid Survey Period", ex.Message);
         }
 
+        [Fact]
+        public void SaveSurveyPeriod_WhenSurveyPeriodIsNotUniqueAndSurveyPeriodIsValid_ShuouldRaiseException()
+        {
+            //ARRANGE
+            
+            surveyPeriodDomainPortIP.Setup<bool>(x => x.IsSurveyPeriodUnique(It.IsAny<SurveyPeriodEntity>())).Returns(false);
+            surveyPeriodDomainPortIP.Setup<bool>(x => x.IsValidSurveyPeriod(It.IsAny<SurveyPeriodEntity>())).Returns(true);
+            
+            var surveyPeriodService = new SurveyPeriodService(surveyPeriodDomainPortIP.Object, surveyPeriodRepository.Object);
+            //ACT
+            var ex = Assert.Throws<SurveyPeriodException>(() => surveyPeriodService.SaveSurveyPeriod(surveyPeriodEntity));
+            //ASSERT
+            Assert.Equal("Invalid Survey Period", ex.Message);
+        }
+
+        [Fact]
+        public void SaveSurveyPeriod_WhenSurveyPeriodIsUniqueAndSurveyPeriodIsNotValid_ShuouldRaiseException()
+        {
+            //ARRANGE
+            surveyPeriodDomainPortIP.Setup<bool>(x => x.IsSurveyPeriodUnique(It.IsAny<SurveyPeriodEntity>())).Returns(true);
+            surveyPeriodDomainPortIP.Setup<bool>(x => x.IsValidSurveyPeriod(It.IsAny<SurveyPeriodEntity>())).Returns(false);
+            
+            var surveyPeriodService = new SurveyPeriodService(surveyPeriodDomainPortIP.Object, surveyPeriodRepository.Object);
+            //ACT
+            var ex = Assert.Throws<SurveyPeriodException>(() => surveyPeriodService.SaveSurveyPeriod(surveyPeriodEntity));
+            //ASSERT
+            Assert.Equal("Invalid Survey Period", ex.Message);
+        }
+
+        [Fact]
+        public void SaveSurveyPeriod_WhenSurveyPeriodIsUniqueAndSurveyPeriodIsValid_ShuouldNotRaiseException()
+        {
+            //ARRANGE
+            surveyPeriodDomainPortIP.Setup<bool>(x => x.IsSurveyPeriodUnique(It.IsAny<SurveyPeriodEntity>())).Returns(true);
+            surveyPeriodDomainPortIP.Setup<bool>(x => x.IsValidSurveyPeriod(It.IsAny<SurveyPeriodEntity>())).Returns(true);
+            
+            surveyPeriodRepository.Setup<bool>(x => x.SaveSurveyPeriod(It.IsAny<SurveyPeriodEntity>())).Returns(true);
+            var surveyPeriodService = new SurveyPeriodService(surveyPeriodDomainPortIP.Object, surveyPeriodRepository.Object);
+            //ACT
+            var result = surveyPeriodService.SaveSurveyPeriod(surveyPeriodEntity);
+            //ASSERT
+            Assert.True(result);
+        }
     }
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -3,3 +3,15 @@ Swagger Nuget is by deafult implemented
 
 YAGNI principle
 (https://jamboard.google.com/d/1YSQylaHARM1yIyguO00bjjG_VmZvhkVqP3fOn6RH6M0/edit?usp=sharing)
+
+
+Auditable Entity ==> Should be composition with value objects
+
+1. Anemic Entity should be discouraged (If a Object does not have any behaviour then it should be trated as value objects or DTO)
+
+2. SurveyPeriodEntity should have the behaviour that realtes to business function of that entity
+	- like Start
+	- Update
+	- Delete
+	- view ??
+3. Unit test case naming convention - Cleared on 4th Oct
